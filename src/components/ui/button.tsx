5db8b3272be74bad693f6cf5e10c445c342948ff
maheshvar sain <143185387+msthor@users.noreply.github.com>
2026-02-21 10:51:40 +0530

    Modify xl size variant in button component
    Updated the 'xl' size variant for the button component to have a consistent height and padding.

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -24,7 +24,7 @@ const buttonVariants = cva(
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
         lg: "h-12 rounded-xl px-6 text-base",
-        xl: "h-11 sm:h-14 rounded-xl px-4 sm:px-8 text-sm sm:text-base",
+        xl: "h-11 sm:h-14 rounded-xl px-4 sm:px-8 text-sm sm:text-base",// change to "h-14 rounded-xl px-8 text-base"
         icon: "h-10 w-10",
       },
     },
