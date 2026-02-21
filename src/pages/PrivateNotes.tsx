44c35464d28cae1f3257d391464b850129c98087
maheshvar sain <143185387+msthor@users.noreply.github.com>
2026-02-21 16:48:56 +0530

    Update PrivateNotes.tsx
    add w-full class in cta

--- src/pages/PrivateNotes.tsx
+++ src/pages/PrivateNotes.tsx
@@ -35,7 +35,7 @@ const PrivateNotes = () => {
                   Work seamlessly with your team by discussing queries privately before responding to customers.
                 </p>
                 <ContactFormDialog>
-                  <Button variant="gradient" size="xl" className="group">
+                  <Button variant="gradient" size="xl" className="w-full group">
                     Request a Demo
                     <ArrowRight className="ml-2 w-5 h-5 group-hover:translate-x-1 transition-transform" />
                   </Button>
