4e1280b74bf34ad0fd7745333944a666c71c1417
maheshvar sain <143185387+msthor@users.noreply.github.com>
2026-02-21 11:09:19 +0530

    Add full width class to sales button

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -356,7 +356,7 @@ const AboutUs = () => {
                     </Button>
                   </ContactFormDialog>
                   <Link to="/contact-us">
-                    <Button variant="hero-outline" size="xl">
+                    <Button variant="hero-outline" size="xl" className="w-full">
                       Talk to Sales
                     </Button>
                   </Link>
