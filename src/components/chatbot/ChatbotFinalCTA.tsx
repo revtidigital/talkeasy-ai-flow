0f9946e592d29fcb6df38c50a86141025470ac82
maheshvar sain <143185387+msthor@users.noreply.github.com>
2026-02-21 11:27:06 +0530

    Update ChatbotFinalCTA.tsx
    add class className="w-full" to get free trial

--- src/components/chatbot/ChatbotFinalCTA.tsx
+++ src/components/chatbot/ChatbotFinalCTA.tsx
@@ -39,7 +39,7 @@ const ChatbotFinalCTA = () => {
 
               <div className="flex flex-col sm:flex-row gap-4 justify-center">
                 <Link to="/contact-us">
-                  <Button variant="hero" size="xl">
+                  <Button variant="hero" size="xl" className="w-full">
                     Get Free Trial
                     <ArrowRight className="w-5 h-5" />
                   </Button>
