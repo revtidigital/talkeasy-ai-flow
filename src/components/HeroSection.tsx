d1760fd134b278bd2ebf4c39ce68e240835ced88
Maheshvar Sain <msthor28@gmail.com>
2026-03-19 17:14:02 +0530

    remove play which one import from lucide-react

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { ArrowRight, Play } from "lucide-react";
+import { ArrowRight } from "lucide-react";
 import ContactFormDialog from "./ContactFormDialog";
 import ChatbotMockup from "./ChatbotMockup";
 
